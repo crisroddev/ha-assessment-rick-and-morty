21ae0b4feb1c7f42690a8b9feeeb419687c387ca
CrisRod <crodriguez85@gmail.com>
2019-07-19 17:09:50 -0400

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from "react";
-import Characters from './componentes/Characters'
+import Characters from './componentes/Characters';
+import Header from './componentes/Header';
 
 export default class App extends Component {
   render() {
     return (
       <Fragment>
+        <Header/>
         <Characters/>
       </Fragment>
     );
